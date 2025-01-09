861ba0f1d4a3f0a985d4194a9b460e75d29fb1b8
Sr-71-Alphaturi <mayank.kapoor2607@gmail.com>
2025-01-09 21:55:16 +0530

    Add error handling, pop-ups, and minor bug fixes

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,21 +1,67 @@
 import PySimpleGUI as sg
-from modules import features
+import layout as lay
+from modules import guifeat
 
 
-layout = [
-    [sg.Text("Enter to do item",background_color="black",border_width=2,justification='center')],
-    [sg.Input(key='INPUT'), sg.Button("Add")]
-]
 sg.theme('gray gray gray')
-window = sg.Window("To-Do App",layout=layout)
+window = sg.Window("To-Do App",layout=lay.Layout)
+
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
-    match event:
-        case sg.WINDOW_CLOSED:
-            break
-        case 'Add':
-            features.add(values['INPUT'])
+    print(event,values)
+    try:
+        match event:
 
+            case sg.WINDOW_CLOSED:
+                break
+            case 'Exit':
+                break
+
+            case 'Add':
+                td = guifeat.read_todo()
+                new_ele = values['INPUT'] + '\n'
+                if new_ele != '\n' and new_ele not in td:
+                    td.append(new_ele)
+                elif new_ele == '\n':
+                    sg.popup_ok("Enter an element",auto_close=True,auto_close_duration=2,title="Error")
+                else:
+                    result = sg.popup_yes_no("Add a duplicate?")
+                    if result == 'Yes':
+                        td.append(new_ele)
+                    else:
+                        continue
+                guifeat.write_todo(td)
+                window['todos'].update(values=td)
+                window['INPUT'].update(value='')
+
+            case 'todos':
+                window['INPUT'].update(value=values['todos'][0].strip('\n'))
+
+            case 'Edit':
+                td = guifeat.read_todo()
+                edit_todo = values['todos'][0]
+                new_todo = values['INPUT']
+                i = td.index(edit_todo)
+                td[i] = new_todo + '\n'
+                guifeat.write_todo(td)
+                window['INPUT'].update(value="")
+                window['todos'].update(values=td)
+
+            case 'Remove':
+                td = guifeat.read_todo()
+                element = values['todos'][0]
+                td.remove(element)
+                guifeat.write_todo(td)
+                window['todos'].update(values=td)
+                window['INPUT'].update(value="")
+
+            case 'Completed':
+                td = guifeat.read_todo()
+                element = values['todos'][0]
+                td.remove(element)
+                guifeat.write_todo(td)
+                window['todos'].update(values=td)
+                window['INPUT'].update(value="")
+    except IndexError as e:
+        sg.popup_ok("Select an element",title="Error",auto_close=True,auto_close_duration=2)
 window.close()
